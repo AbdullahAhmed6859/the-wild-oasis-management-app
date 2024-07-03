f97affbcd42b3cbb5dad9ec7de244c6a5cb08d88
AbdullahAhmed6859 <109270476+AbdullahAhmed6859@users.noreply.github.com>
2024-07-03 18:50:20 +0500

    supabase setup

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  const [cabins, setCabins] = useState({});
+  useEffect(function () {
+    getCabins().then((data) => setCabins(data));
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
+      <p>{JSON.stringify(cabins)}</p>
+      <img
+        src="https://grsippozsesugdalgnst.supabase.co/storage/v1/object/public/cabin-images/cabin_001.jpg"
+        alt=""
+      />
     </Row>
   );
 }
---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://grsippozsesugdalgnst.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdyc2lwcG96c2VzdWdkYWxnbnN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk4NDAzMTgsImV4cCI6MjAzNTQxNjMxOH0.oXj--mfrpgTDKlm1W65FzzIm-4GhtW1J37yKGfK-Cac";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
